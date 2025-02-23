923fdd6a7d9ecdc5580db3596c83f4cfd2f65f1e
Kam Kennedy <kameronk013@gmail.com>
2025-02-23 09:23:10 -0700

    init fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,16 @@
+// When a user inputs a number
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+// Loop from 1 to the entered number
+
+
+// If the current number is divisible by 3 then print "Fizz"
+
+
+// If the current number is divisible by 5 then print "Buzz"
+
+
+// If the current number is divisible by 3 and 5 then print "FizzBuzz"
+
+
+// Otherwise print the current number
