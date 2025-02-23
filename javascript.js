e58957dde3ba5e6569ee94e0c6ee02577b773fe6
Kam Kennedy <kameronk013@gmail.com>
2025-02-23 08:56:27 -0700

    function practice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,29 @@
-"what the fuck is up denny's"
+function favoriteAnimal(animal) {
+  return "My favorite animal is a " + animal;
+}
+
+console.log(favoriteAnimal("dog"));
+
+function add7(num) {
+  return num + 7;
+}
+
+console.log(add7(10))
+
+function multiply(num1, num2){
+  return num1 * num2;
+}
+
+console.log(multiply(9, 7))
+
+function capitalize(string){
+  return string[0].toUpperCase() + string.slice(1).toLowerCase();
+}
+
+console.log(capitalize("murphy"))
+
+function lastLetter(string) {
+  return string[string.length - 1];
+}
+
+console.log(lastLetter("murphy"))
